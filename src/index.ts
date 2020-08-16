2ca8d8cab48c27852b33eeff506175e43d4d4ebc
pacokwon <haechank@gmail.com>
2020-08-17 02:08:53 +0900

    update: refactor from class to conventional express app

--- src/index.ts
+++ src/index.ts
@@ -1,40 +1,25 @@
 import express from 'express';
-import mongoose from 'mongoose';
-
-import { User, UserModel } from './models/user';
+import mongoose, { createConnection } from 'mongoose';
+import cors from 'cors';
 import { DB } from './models/db';
 
-class App {
-  public application: express.Application;
-
-  constructor() {
-    this.application = express();
-    this.router();
-  }
-
-  private router(): void {
-    this.application.get('/', (req: express.Request, res: express.Response) => {
-      res.send('hello!');
-    });
-  }
-}
-
 const uri = 'mongodb://localhost/kapoera';
-
-const db = new DB();
-let testUser = {
-  key: 1,
-  score: 500,
-  is_admin: true,
-  nickname: 'alogon',
-  name: 'geonho',
-  department: 'CS',
-  student_number: 12345678,
-  password: 'password'
-};
-
 mongoose.connect(uri, { useNewUrlParser: true });
-UserModel.create(testUser);
+const db = new DB();
 
-const app = new App().application;
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use('/api', authMiddleware);
+
+app.get('/', (req: express.Request, res: express.Response) => {
+  res.send('hello!');
+});
+
+app.post('/auth/login', (req: express.Request, res: express.Response) => {
+});
+app.post('/api/nickname', (req: express.Request, res: express.Response) => {
+});
+
 app.listen(3000, () => console.log('start'));
