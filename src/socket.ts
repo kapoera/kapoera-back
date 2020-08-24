7c599b62e15636208a4c33c177bac0ee82db3c16
alogon <ghlee5084@kaist.ac.kr>
2020-08-24 16:48:01 +0000

    refresh info using socket server

--- src/socket.ts
+++ src/socket.ts
@@ -4,23 +4,43 @@ import { gameType } from './models/game';
 
 const socketEvents = (io: Server) => {
   io.on('connection', (socket: Socket) => {
-    console.log('test');
-  });
-  const quiz = io.of('/quiz');
-  quiz.on('connection', (socket: Socket) => {
-    console.log('in');
-    const refreshInfo = () => {
-      db.readGame(<gameType>'quiz').then(game => {
-        socket.emit('refresh', game[0]);
-      });
-    };
-    const refreshFront = setInterval(refreshInfo, 1000);
+    console.log(socket.handshake.query.game);
+    socket.join(socket.handshake.query.game);
 
     socket.on('disconnect', () => {
       console.log('out');
-      clearInterval(refreshFront);
     });
   });
+
+  const refreshInfo = () => {
+    const games = ['quiz', 'ai', 'lol', 'kart', 'hacking'];
+    games.forEach(game_type => {
+      db.readGame(<gameType>game_type)
+        .then(game => {
+          io.sockets.in(game_type).emit('refresh', game[0]);
+        })
+        .catch(err => {
+          console.log(err.message);
+        });
+    });
+  };
+
+  setInterval(refreshInfo, 1000);
+  //   const quiz = io.of('/quiz');
+  //   quiz.on('connection', (socket: Socket) => {
+  //     console.log('in');
+  //     const refreshInfo = () => {
+  //       db.readGame(<gameType>'quiz').then(game => {
+  //         socket.emit('refresh', game[0]);
+  //       });
+  //     };
+  //     const refreshFront = setInterval(refreshInfo, 1000);
+
+  //     socket.on('disconnect', () => {
+  //       console.log('out');
+  //       clearInterval(refreshFront);
+  //     });
+  //   });
 };
 
 export default socketEvents;
