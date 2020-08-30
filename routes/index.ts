ef074f19b46e0d814c78c4e100a726adb4a05ca2
pacokwon <haechank@gmail.com>
2020-08-30 18:38:11 +0900

    update: move types + update user model type

--- routes/index.ts
+++ routes/index.ts
@@ -2,34 +2,11 @@ import express from 'express';
 import authRouter from './auth';
 import apiRouter from './api';
 import crypto from 'crypto';
+import { SSOResult, SSODataMap, SSOUserInfo } from '../utils/type';
 
 const router = express.Router();
 router.use('/auth', authRouter);
 router.use('/api', apiRouter);
-
-interface SSOResult {
-  result: string;
-  k_uid: string;
-  enc: string;
-  success: string;
-  user_id: string;
-  state: string;
-}
-
-interface SSODataMap {
-  USER_INFO: {
-    ku_std_no: string;
-    uid: string;
-    kaist_uid: string;
-    mail: string;
-    givenname: string;
-    mobile: string;
-    ku_kname: string;
-    sn: string;
-  };
-  state: string;
-  REDIRECT_URL: string;
-}
 
 const decrypt = (encrypted: string, keySpec: Buffer, iv: Buffer) => {
   const decipher = crypto.createDecipheriv('aes-128-cbc', keySpec, iv);
--- utils/type.ts
+++ utils/type.ts
@@ -15,3 +15,29 @@ export interface LoginInput {
   username: string;
   password: string;
 }
+
+export interface SSOResult {
+  result: string;
+  k_uid: string;
+  enc: string;
+  success: string;
+  user_id: string;
+  state: string;
+}
+
+export interface SSOUserInfo {
+  ku_std_no: string;
+  uid: string;
+  kaist_uid: string;
+  mail: string;
+  givenname: string;
+  mobile: string;
+  ku_kname: string;
+  sn: string;
+}
+
+export interface SSODataMap {
+  USER_INFO: SSOUserInfo;
+  state: string;
+  REDIRECT_URL: string;
+}
