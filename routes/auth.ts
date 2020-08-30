f741f3c36beb333c4afb79f6c596935bd80006ae
pacokwon <haechank@gmail.com>
2020-08-31 02:41:05 +0900

    remove: deprecated login endpoint

--- routes/auth.ts
+++ routes/auth.ts
@@ -7,46 +7,6 @@ import * as db from '../src/models/db';
 
 const router = express.Router();
 
-router.post('/login', (req: express.Request, res: express.Response) => {
-  db.readUser(req.body.username).then(user => {
-    if (user.length > 0) {
-      const { _id, __v, password, ...userinfo } = user[0].toObject();
-      jwtUtils
-        .createTokens(userinfo.username)
-        .then((tokens: Tokens) => {
-          res.json({
-            success: true,
-            accessToken: tokens.accessToken,
-            refreshToken: tokens.refreshToken,
-            is_new: false,
-            userinfo
-          });
-        })
-        .catch(err => res.json({ success: false, message: err.message }));
-    } else {
-      db.createUser(<LoginInput>req.body)
-        .then(saveUser => {
-          const { _id, __v, password, ...userinfo } = saveUser.toObject();
-          jwtUtils
-            .createTokens(saveUser.username)
-            .then((tokens: Tokens) => {
-              res.json({
-                success: true,
-                accessToken: tokens.accessToken,
-                refreshToken: tokens.refreshToken,
-                is_new: true,
-                userinfo
-              });
-            })
-            .catch(err => res.json({ success: false, message: err.message }));
-        })
-        .catch(err => {
-          res.json({ success: false, message: err.message });
-        });
-    }
-  });
-});
-
 router.post('/login-sso', (req: express.Request, res: express.Response) => {
   const state = Date.now().toString();
   req.session.state = state;
