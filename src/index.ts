535331ece470e407fd1f546c490eee30db3f97c5
pacokwon <haechank@gmail.com>
2020-08-30 13:17:51 +0900

    add: .env.example file + initialize dotenv

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,14 @@ import express, { Router } from 'express';
 import mongoose, { createConnection } from 'mongoose';
 import morgan from 'morgan';
 import cors from 'cors';
+import dotenv from 'dotenv';
 import router from '../routes';
 import { JwtDecodedInfo } from '../utils/type';
 import * as db from './models/db';
 import SocketIo from 'socket.io';
 import socketEvents from './socket';
 
+dotenv.config();
 const uri = 'mongodb://localhost/kapoera';
 mongoose.connect(uri, { useNewUrlParser: true, useFindAndModify: false });
 
