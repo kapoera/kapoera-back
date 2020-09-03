7fde9ad90582c91874fc5e772cee9297e88e6ce4
pacokwon <haechank@gmail.com>
2020-09-04 00:03:28 +0900

    add: rankings endpoint

--- routes/api.ts
+++ routes/api.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import * as db from '../src/models/db';
 import privateRouter from './private';
 import { gameType } from '../src/models/game';
+import { UserModel } from '../src/models/user';
 
 const router = express.Router();
 
@@ -29,14 +30,33 @@ router.get(
 );
 
 router.get(
-'/events/:gameType', async (req: express.Request, res: express.Response) => {
-  console.log(req.params.gameType)
-  await db.readEvent(<gameType>req.params.gameType)
-  .then(events => {
-    res.json(events.map(event => event.toObject()))
-  })
-  .catch(err => {
-    res.json({ message: err.message });
-  });
+  '/events/:gameType', async (req: express.Request, res: express.Response) => {
+    await db.readEvent(<gameType>req.params.gameType)
+      .then(events => {
+        res.json(events.map(event => event.toObject()))
+      })
+      .catch(err => {
+        res.json({ message: err.message });
+      });
 })
+
+router.get(
+  '/rankings/top',
+  async (req: express.Request, res: express.Response) => {
+    const { limit = 5 } = req.query;
+
+    try {
+      const ranking = await UserModel.aggregate([
+        { $project: { nickname: 1, score: 1, _id: 0 } },
+        { $sort: { score: -1 } },
+        { $limit: limit }
+      ]);
+
+      res.json({ success: true, ranking });
+    } catch (error) {
+      res.json({ success: false });
+    }
+  }
+);
+
 export default router;
