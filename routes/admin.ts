86c99fc7117c09fb47f12c839e00d2bdfeb716a2
alogon <ghlee5084@kaist.ac.kr>
2020-09-05 15:28:42 +0900

    add end-game endpoint

--- routes/admin.ts
+++ routes/admin.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import * as db from '../src/models/db';
 import { gameType, GameModel, playingType } from '../src/models/game';
 import { adminMiddleware } from '../middlewares/auth';
+import { UserModel } from '../src/models/user';
 
 const router = express.Router();
 
@@ -33,19 +34,36 @@ router.post(
   }
 );
 
-// router.post(
-//   '/end-game',
-//   async (req: express.Request, res: express.Response) => {
-//     const { playing, game_type } = req.body;
-//     if (playing) {
-//       try {
-//         await GameModel.findOneAndUpdate({ game_type }, { playing });
-//         res.json({ success: true });
-//       } catch (error) {
-//         res.json({ success: false, message: error.message });
-//       }
-//     } else res.json({ success: false });
-//   }
-// );
+router.post(
+  '/end-game',
+  async (req: express.Request, res: express.Response) => {
+    const { winner, game_type } = req.body;
+    if (winner) {
+      try {
+        await GameModel.findOne({ game_type }).then(async game => {
+          if (game === null) res.json({ success: false });
+          else {
+            if (winner === 'K') {
+              const scoreEach = game.dividend / game.kaist_arr.length;
+              await UserModel.updateMany(
+                { _id: { $in: game.kaist_arr } },
+                { $inc: { score: scoreEach } }
+              );
+            } else if (winner === 'P') {
+              const scoreEach = game.dividend / game.postech_arr.length;
+              await UserModel.updateMany(
+                { _id: { $in: game.postech_arr } },
+                { $inc: { score: scoreEach } }
+              );
+            }
+            res.json({ success: true });
+          }
+        });
+      } catch (error) {
+        res.json({ success: false, message: error.message });
+      }
+    } else res.json({ success: false });
+  }
+);
 
 export default router;
