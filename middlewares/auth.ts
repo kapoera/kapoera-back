e2da7497c03667e3eff0f5efb67e37c659ecffa1
pacokwon <haechank@gmail.com>
2020-09-06 22:26:47 +0900

    remove: comments in middleware

--- middlewares/auth.ts
+++ middlewares/auth.ts
@@ -28,34 +28,34 @@ export function authMiddleware(
       message: error.message
     });
   }
-  next();
-  // if (req.cookies['kapoera-access'] === undefined) {
-  //   if (req.cookies['kapoera-refresh'] === undefined) {
-  //     res.status(401).send('Refresh Token Expired');
-  //   } else {
-  //     const decoded = jwt.decode(req.cookies['kapoera-refresh']);
-  //     createAccessToken((<{ mail: string }>decoded).mail).then(token => {
-  //       req.decoded = jwt.decode(token) as JwtDecodedInfo;
 
-  //       res.cookie('kapoera-access', token, {
-  //         expires: new Date(Date.now() + 15 * 60 * 1000), // 15 minutes
-  //         httpOnly: true,
-  //         secure: true
-  //       });
+  if (req.cookies['kapoera-access'] === undefined) {
+    if (req.cookies['kapoera-refresh'] === undefined) {
+      res.status(401).send('Refresh Token Expired');
+    } else {
+      const decoded = jwt.decode(req.cookies['kapoera-refresh']);
+      createAccessToken((<{ mail: string }>decoded).mail).then(token => {
+        req.decoded = jwt.decode(token) as JwtDecodedInfo;
 
-  //       next();
-  //     });
-  //   }
-  // } else {
-  //   auth(req.cookies['kapoera-access'])
-  //     .then(decoded => {
-  //       if (decoded !== undefined) {
-  //         req.decoded = decoded as JwtDecodedInfo;
-  //       }
-  //       next();
-  //     })
-  //     .catch(onError);
-  // }
+        res.cookie('kapoera-access', token, {
+          expires: new Date(Date.now() + 15 * 60 * 1000), // 15 minutes
+          httpOnly: true,
+          secure: true
+        });
+
+        next();
+      });
+    }
+  } else {
+    auth(req.cookies['kapoera-access'])
+      .then(decoded => {
+        if (decoded !== undefined) {
+          req.decoded = decoded as JwtDecodedInfo;
+        }
+        next();
+      })
+      .catch(onError);
+  }
 }
 
 export async function adminMiddleware(
@@ -63,18 +63,16 @@ export async function adminMiddleware(
   res: express.Response,
   next: express.NextFunction
 ): Promise<void> {
-  next();
-  console.log('adminMiddleware');
-  // const { mail } = req.decoded;
+  const { mail } = req.decoded;
 
-  // try {
-  //   const user = await UserModel.findOne({ mail });
-  //   if (user === null) res.status(400).send('User does not exist');
-  //   else if (!user.is_admin) res.status(400).send('User is not admin');
-  //   else {
-  //     next();
-  //   }
-  // } catch (error) {
-  //   res.status(500).send(error);
-  // }
+  try {
+    const user = await UserModel.findOne({ mail });
+    if (user === null) res.status(400).send('User does not exist');
+    else if (!user.is_admin) res.status(400).send('User is not admin');
+    else {
+      next();
+    }
+  } catch (error) {
+    res.status(500).send(error);
+  }
 }
